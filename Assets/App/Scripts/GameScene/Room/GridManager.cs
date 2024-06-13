66a9a35f7e5c0ee34a36645743d4d15edfa08334
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-06-13 16:01:29 +0300

    optimize floor + made train correct colliders

--- Assets/App/Scripts/GameScene/Room/GridManager.cs
+++ Assets/App/Scripts/GameScene/Room/GridManager.cs
@@ -5,6 +5,14 @@ using Random = UnityEngine.Random;
 
 namespace App.Scripts.GameScene.Room
 {
+    public enum ExitPosition
+    {
+        Top,
+        Bottom,
+        Left,
+        Right
+    }
+
     public class GridManager : MonoBehaviour
     {
         [SerializeField] private GameObject defaultGridPrefab;
@@ -13,12 +21,12 @@ namespace App.Scripts.GameScene.Room
         [SerializeField] private GameObject windowPrefab;
         [SerializeField] Vector2Int gridSize;
         [SerializeField] private GameObject[] exitVariants;
+        [SerializeField] private ExitPosition exitPosition;
 
         private Vector3 _gridInitialPosition;
         private float _wallOffsetY;
         private float _windowOffsetY;
-        
-    
+
         void Start()
         {
            // GenerateGrid();
@@ -30,11 +38,25 @@ namespace App.Scripts.GameScene.Room
             _wallOffsetY = wallPrefab.transform.localScale.y / 2;
             _windowOffsetY = windowPrefab.transform.localScale.y / 2;
         }
+
         [Button]
         private void GenerateGrid()
         {
             ClearGrid();
             RoomParametersSetup();
+
+            Vector3 floorPosition = new Vector3(_gridInitialPosition.x + (gridSize.x - 1) / 2.0f, _gridInitialPosition.y, _gridInitialPosition.z + (gridSize.y - 1) / 2.0f);
+            GameObject floorObj = Instantiate(defaultGridPrefab, floorPosition, Quaternion.identity);
+            floorObj.transform.localScale = new Vector3(gridSize.x, 0.1f, gridSize.y);
+            floorObj.transform.parent = transform;
+
+            Renderer floorRenderer = floorObj.GetComponent<Renderer>();
+            if (floorRenderer != null)
+            {
+               floorRenderer.sharedMaterial.mainTextureScale = new Vector2(gridSize.x, gridSize.y);
+
+            }
+
             for (int x = -1; x <= gridSize.x; x++)
             {
                 for (int z = -1; z <= gridSize.y; z++)
@@ -43,30 +65,35 @@ namespace App.Scripts.GameScene.Room
                     {
                         Vector3 wallPosition = new Vector3(_gridInitialPosition.x + x, _gridInitialPosition.y, _gridInitialPosition.z + z);
                         GameObject wallObj;
-                        if ((x == gridSize.x - 1 && z == gridSize.y) /*|| (x == gridSize.x && z == gridSize.y - 1)*/)
+
+                        bool isDoorPosition = false;
+                        switch (exitPosition)
+                        {
+                            case ExitPosition.Top:
+                                isDoorPosition = z == gridSize.y && x == gridSize.x / 2;
+                                break;
+                            case ExitPosition.Bottom:
+                                isDoorPosition = z == -1 && x == gridSize.x / 2;
+                                break;
+                            case ExitPosition.Left:
+                                isDoorPosition = x == -1 && z == gridSize.y / 2;
+                                break;
+                            case ExitPosition.Right:
+                                isDoorPosition = x == gridSize.x && z == gridSize.y / 2;
+                                break;
+                        }
+
+                        if (isDoorPosition)
                         {
                             windowPrefab = exitVariants[Random.Range(0, exitVariants.Length)];
-                            wallObj = Instantiate(windowPrefab, wallPosition + Vector3.up * _windowOffsetY, Quaternion.identity); 
+                            wallObj = Instantiate(windowPrefab, wallPosition + Vector3.up * _windowOffsetY, Quaternion.identity);
                         }
                         else
                         {
                             wallObj = Instantiate(wallPrefab, wallPosition + Vector3.up * _wallOffsetY, Quaternion.identity);
                         }
+
                         wallObj.transform.parent = transform;
-                    }
-                    else
-                    {
-                        Vector3 gridPosition = new Vector3(_gridInitialPosition.x + x, _gridInitialPosition.y, _gridInitialPosition.z + z);
-                        GameObject gridObj;
-                        if (x == gridSize.x - 1 && z == gridSize.y - 1)
-                        {
-                            gridObj = Instantiate(exitGridPrefab, gridPosition, Quaternion.identity); 
-                        }
-                        else
-                        {
-                            gridObj = Instantiate(defaultGridPrefab, gridPosition, Quaternion.identity);
-                        }
-                        gridObj.transform.parent = transform;
                     }
                 }
             }
